c6f054ff486c2966589bb97d257e678e59dc905f
Andrew Nickerson <andrew.nickerson@chimp.net>
2017-10-29 19:09:06 -0600

    WIP Transaction table styling, Highlighting credit lines

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,52 +1,61 @@
 import React from 'react';
 import _ from 'lodash';
+import classNames from 'classnames';
+import './transactionTable.css';
 
 const TransactionRow = (props) => {
-	const {
-		Amount,
-		Company,
-		Date,
-		Ledger,
-	} = props;
+  const {
+    Amount,
+    Company,
+    Date,
+    Ledger,
+  } = props;
 
-	return (
-		<tr className="transaction-table_row">
-			<td>{Date}</td>
-			<td>{Company}</td>
-			<td>{Ledger}</td>
-			<td>{Amount}</td>
-		</tr>
-	);
+  const className = classNames(
+    'transaction-table_row',
+    {
+      'transaction-table_row--credit': (Amount > 0),
+    }
+  );
+
+  return (
+    <tr className={className}>
+      <td>{Date}</td>
+      <td>{Company}</td>
+      <td>{Ledger}</td>
+      <td>{Amount}</td>
+    </tr>
+  );
 }
 
 const TransactionHeader = (props) => {
-	return (
-		<thead className="transaction-table_header">
-	    <tr>
-				<th>Date</th>
-				<th>Company</th>
-				<th>Ledger</th>
-				<th>{props.total}</th>
-	    </tr>
-  	</thead>
-	);
+  return (
+    <thead className="transaction-table_header">
+        <tr>
+          <th>Date</th>
+          <th>Company</th>
+          <th>Ledger</th>
+          <th>{props.total}</th>
+        </tr>
+      </thead>
+  );
 }
 
 const TransactionTable = (props) => {
-	const {
-		total,
-		transactions,
-	} = props;
+  const {
+    total,
+    transactions,
+  } = props;
 
-	return (
-		<table className="transaction-table">
-			<TransactionHeader total={total}/>
-			{_.map(
-				transactions, 
-				(transaction) => <TransactionRow {...transaction}/>,
-			)}
-		</table>
-	);
+  return (
+    <table className="transaction-table">
+      <TransactionHeader total={total}/>
+      {_.map(
+        transactions, 
+        (transaction) => <TransactionRow {...transaction}/>,
+      )}
+    </table>
+  );
 }
 
 export default TransactionTable;
